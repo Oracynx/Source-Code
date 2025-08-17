e264d25fe139f1f795e3bbb4a5e162b49baff192
Hongtian Wang <oracynx@gmail.com>
2025-08-17 08:03:38 +0800

    :sparkles: CQYC0727

---
+++ CQYC/2025/0727/C.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+constexpr int MaxN = 3e5 + 5;
+int n, m, b;
+int p[MaxN];
+int main()
+{
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= 1 << n; i++)
+    {
+    }
+    b = std::sqrt((1 << n) * n);
+    return 0;
+}
