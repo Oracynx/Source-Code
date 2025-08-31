b5d9bdea81d1810461d3238ac3378bb6f48bd198
Hongtian Wang <oracynx@gmail.com>
2025-08-31 09:36:37 +0800

    :sparkles: P1025

--- Problem/Luogu/P1025.cpp
+++ Problem/Luogu/P1025.cpp
@@ -2,29 +2,19 @@
 int n, k;
 int dfs(int n, int k)
 {
-    if (k == 0)
+    if (n < k)
     {
         return 0;
     }
-    else if (n == 0 || k == 1)
+    if (k == 1 || n == k)
     {
         return 1;
     }
-    else
-    {
-        if (n >= k)
-        {
-            return dfs(n - k, k) + dfs(n, k - 1);
-        }
-        else
-        {
-            return dfs(n, k - 1);
-        }
-    }
+    return dfs(n - k, k) + dfs(n - 1, k - 1);
 }
 int main()
 {
     scanf("%d%d", &n, &k);
-    printf("%d", dfs(n, k) / 2);
+    printf("%d", dfs(n, k));
     return 0;
 }
